efffc65d4caa3e5416434de5da921683208b44d4
Gensias <45399046+Gensias@users.noreply.github.com>
2021-12-31 10:03:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=7Xr5NR6HyW6RWvFhLqKhT3; device_id=3395957798161552052107487783dc62c3e5847d55e50bc5e00e50997d; _ga=GA1.2.898293334.1615520522; __gads=ID=e585fb8b54b9cf0e:T=1615520522:S=ALNI_Ma7aZJT6ZMBIleYXJb-0uEYWDrIlw; homepage_sug=c; r_sort_type=score; sess=AT-nY/L5lbDhOH1C9aDpiFfVes4yPHMXXBQ2yNQ/lHoT7EPS4ZsqF7TPrH6ZQphnEGN0Q5ry9bj6QZCGLhqNpA1tEvN/XIQPHzu7XSc+deDfjULIhvFJBYLbpjN; user=user:2812632101|2812632101; smzdm_id=2812632101; smzdm_user_view=8817871337C647B736C8F61A6C3AEE17; smzdm_user_source=0C7BDE55E134858CA80EFCB6571E4186; __jsluid_s=85a63aa1e8e08912ea7d2a9fb56adbfd; DISABLE_APP_TIP=1; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1639978459,1640915502,1640915987; _zdmA.uid=ZDMA.nysI-1E5e.1640915987.2419200; sensorsdata2015jssdkcross={"distinct_id":"2812632101","first_id":"1782486db8eddf-0197ef8cf5cdeb-3a7b085f-2073600-1782486db906c9","props":{"$latest_traffic_source_type":"引荐流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://github.com/Gensias/smzdm_bot","$latest_landing_page":"https://www.smzdm.com/"},"$device_id":"1782486db8eddf-0197ef8cf5cdeb-3a7b085f-2073600-1782486db906c9"}; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1640916070'
 
 
 """
